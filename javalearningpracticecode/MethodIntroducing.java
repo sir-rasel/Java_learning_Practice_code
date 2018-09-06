5f44d52951cabfce30ee81f739ea61da3e8359a7
Saiful Islam Rasel <raselcse97@gmail.com>
2018-09-06 16:01:46 +0600

    Java method intrducing

---
+++ javalearningpracticecode/MethodIntroducing.java
@@ -0,0 +1,20 @@
+
+package javalearningpracticecode;
+import java.util.Scanner;
+
+public class MethodIntroducing {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Input two number: ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        
+        System.out.println("Sum of a and b: ");
+        int sum = add(a,b);
+    }
+    
+    static int add(int a,int b){
+        return a+b;
+    }
+}
